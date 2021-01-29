5d4e0ec9b8cff2973061eb23ab11c937901bd3ed
zrpnr <peter.weber@acquia.com>
2021-01-29 14:53:22 -0700

    Widgetize simplebox

--- src/simplebox/simpleboxediting.js
+++ src/simplebox/simpleboxediting.js
@@ -1,6 +1,12 @@
 import { Plugin } from 'ckeditor5/src/core';
+import { toWidget, toWidgetEditable } from 'ckeditor5/src/widget';
+import { Widget } from 'ckeditor5/src/widget';
 
 export default class SimpleBoxEditing extends Plugin {
+  static get requires() {
+    return [Widget];
+  }
+
   init() {
     this._defineSchema();
     this._defineConverters();
@@ -29,7 +35,7 @@ export default class SimpleBoxEditing extends Plugin {
   _defineConverters() {
     const conversion = this.editor.conversion;
 
-    conversion.elementToElement({
+    conversion.for('upcast').elementToElement({
       model: 'simpleBox',
       view: {
         name: 'section',
@@ -37,7 +43,26 @@ export default class SimpleBoxEditing extends Plugin {
       },
     });
 
-    conversion.elementToElement({
+    conversion.for('dataDowncast').elementToElement({
+      model: 'simpleBox',
+      view: {
+        name: 'section',
+        classes: 'simple-box',
+      },
+    });
+
+    conversion.for('editingDowncast').elementToElement({
+      model: 'simpleBox',
+      view: (modelElement, { writer: viewWriter }) => {
+        const section = viewWriter.createContainerElement('section', {
+          class: 'simple-box',
+        });
+
+        return toWidget(section, viewWriter, { label: 'simple box widget' });
+      },
+    });
+
+    conversion.for('upcast').elementToElement({
       model: 'simpleBoxTitle',
       view: {
         name: 'h1',
@@ -45,11 +70,47 @@ export default class SimpleBoxEditing extends Plugin {
       },
     });
 
-    conversion.elementToElement({
+    conversion.for('dataDowncast').elementToElement({
+      model: 'simpleBoxTitle',
+      view: {
+        name: 'h1',
+        classes: 'simple-box-title',
+      },
+    });
+
+    conversion.for('editingDowncast').elementToElement({
+      model: 'simpleBoxTitle',
+      view: (modelElement, { writer: viewWriter }) => {
+        const h1 = viewWriter.createEditableElement('h1', {
+          class: 'simple-box-title',
+        });
+        return toWidgetEditable(h1, viewWriter);
+      },
+    });
+
+    conversion.for('upcast').elementToElement({
       model: 'simpleBoxDescription',
       view: {
         name: 'div',
         classes: 'simple-box-description',
+      },
+    });
+
+    conversion.for('dataDowncast').elementToElement({
+      model: 'simpleBoxDescription',
+      view: {
+        name: 'div',
+        classes: 'simple-box-description',
+      },
+    });
+
+    conversion.for('editingDowncast').elementToElement({
+      model: 'simpleBoxDescription',
+      view: (modelElement, { writer: viewWriter }) => {
+        const div = viewWriter.createEditableElement('div', {
+          class: 'simple-box-description',
+        });
+        return toWidgetEditable(div, viewWriter);
       },
     });
   }
